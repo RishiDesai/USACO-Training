2104ebce9de240662a83dc8ce93f474713114707
Rishi Desai <rishidesai000@gmail.com>
2016-02-13 19:22:08 -0800

    optimal solutions

--- 1.2/milk2.java
+++ 1.2/milk2.java
@@ -7,8 +7,6 @@ TASK: milk2
 package chapter1.sec2;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class milk2 {
@@ -19,54 +17,40 @@ public class milk2 {
 
         final int N = Integer.parseInt(br.readLine());
 
-        int[][] farmers = new int[N][2];
-        int[] lows = new int[N];
-        int[] highs = new int[N];
+        boolean[] time = new boolean[1000000 + 1];
+        int start = 1 << 30, end = - 1;
+
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int finish = Integer.parseInt(st.nextToken());
-            lows[i] = start;
-            highs[i] = finish;
-            farmers[i] = new int[]{start, finish};
+
+            int low = Integer.parseInt(st.nextToken());
+            int high = Integer.parseInt(st.nextToken());
+
+            for (int t = low; t < high; t++) {
+                time[t] = true;
+            }
+
+            if (low < start) start = low;
+            if (high > end) end = high;
         }
 
-        Arrays.sort(farmers, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];
-            }
-        });
-        Arrays.sort(lows);
-        Arrays.sort(highs);
-
-        final int low = lows[0];
-        final int high = highs[highs.length - 1];
-
-        boolean[] time = new boolean[high];
-
-        for (int[] farmer : farmers)
-            for (int t = farmer[0]; t < farmer[1]; t++)
-                time[t] = true;
-
-
-        int maxMilking = 0, maxNotMilking = 0;
-        int milking = 0, notMilking = 0;
-        for (int i = low; i < high; i++) {
+        int maxIs = 0, maxNot = 0;
+        int is = 0, not = 0;
+        for (int i = start; i < end; i++) {
 
             if (time[i]) {
-                milking++;
-                notMilking = 0;
+                is++;
+                not = 0;
             } else {
-                notMilking++;
-                milking = 0;
+                not++;
+                is = 0;
             }
-            maxNotMilking = Math.max(maxNotMilking, notMilking);
-            maxMilking = Math.max(maxMilking, milking);
+            if (is > maxIs) maxIs = is;
+            if (not > maxNot) maxNot = not;
+
         }
 
-        pw.println(maxMilking + " " + maxNotMilking);
-        System.out.println(maxMilking + " " + maxNotMilking);
+        pw.println(maxIs + " " + maxNot);
         pw.close();
     }
 
--- 1.3/milk.java
+++ 1.3/milk.java
@@ -8,99 +8,62 @@ package chapter1.sec3;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class milk {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("milk.in"));
-        PrintWriter pw = new PrintWriter(new File("milk.out"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("milk.out")));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        final int N = Integer.parseInt(st.nextToken());
+        final int M = Integer.parseInt(st.nextToken());
 
-        final int milkNeeded = Integer.parseInt(st.nextToken()); // amount company needs
-        final int clients = Integer.parseInt(st.nextToken()); // amount of farmers needed
+        Farmer[] fs = new Farmer[M];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
 
-        Farmer[] farmers = new Farmer[clients];
+            int price = Integer.parseInt(st.nextToken());
+            int amount = Integer.parseInt(st.nextToken());
 
-        String line;
-        int j = 0;
-        while ((line = br.readLine()) != null) {
-            int price = Integer.parseInt(line.split(" ")[0]);
-            int max = Integer.parseInt(line.split(" ")[1]);
-            farmers[j] = new Farmer(price, max);
-            j++;
+            fs[i] = new Farmer(price, amount);
         }
 
-        Arrays.sort(farmers, new Comparator<Farmer>() {
-            @Override
-            public int compare(Farmer o1, Farmer o2) {
-                if (o1.price > o2.price)
-                    return 1;
-                else if (o1.price < o2.price)
-                    return - 1;
-                else
-                    return 0;
-            }
-        });
+        Arrays.sort(fs);
 
-        int milk = 0;
-        int paid = 0;
-
-        for (Farmer farmer : farmers) {
-            if (milk < milkNeeded) {
-                if (farmer.getMax() + milk >= milkNeeded) {
-                    int difference = milkNeeded - milk;
-                    milk += difference;
-                    paid += difference * farmer.getPrice();
-                    break;
-                } else {
-                    milk += farmer.getMax();
-                    paid += farmer.getMax() * farmer.getPrice();
-
-                }
+        int milk = 0, cost = 0;
+        for (Farmer f : fs) {
+            if (milk + f.max <= N) {
+                milk += f.max;
+                cost += f.price * f.max;
             } else {
+                int diff = N - milk;
+                cost += f.price * diff;
                 break;
             }
         }
 
-        pw.println(paid);
-        System.out.println(paid);
-
+        pw.println(cost);
         pw.close();
-        System.exit(0);
     }
 
-    private static class Farmer {
+    private static class Farmer implements Comparable<Farmer> { // immutable
 
-        private final int price;
-        private int max;
+        protected final int price, max;
 
-        public Farmer(int price, int max) {
+        protected Farmer(int price, int max) {
             this.price = price;
             this.max = max;
-
         }
 
-        public String toString() {
-            return "[" + price + " " + max + "]";
+        @Override
+        public int compareTo(Farmer f) {
+            if (price > f.price) return 1;
+            if (price < f.price) return - 1;
+            return 0;
         }
 
-        public int getPrice() {
-            return this.price;
-        }
-
-        public int getMax() {
-            return this.max;
-        }
-
-    }
-
-    private static void print(Object[] array) {
-        for (Object o : array) {
-            System.out.println(o);
-        }
-        System.out.println();
     }
 
 }
